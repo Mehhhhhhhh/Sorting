7111c71bbbec69127306c62f18401802d8f7fcfd
~j <jmarcelj@me.com>
2017-04-23 07:36:59 -0400

    updated drop_first variable initialization to one line

--- Sources/SelectionSort.swift
+++ Sources/SelectionSort.swift
@@ -39,8 +39,7 @@ struct SelectionSort<T: Comparable> {
 
   func tail(min: inout (Int,T), head i: Int, sequence: ArraySlice<T>) ->
     (Int,T) {
-    var drop_first = sequence.count-(original.count-i)
-    drop_first = max(0, drop_first)
+    let drop_first = max(0, sequence.count-(original.count-i))
     guard let (head, tail) = sequence.dropFirst(drop_first).decompose()
       else { return min }
     if head < min.1 {
